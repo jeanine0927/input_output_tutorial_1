de9ede74cbac489e3a046cdbc775919ee51f4073
Jeanine Cheng <jeaninecheng@Jeanines-MacBook-Air.local>
2023-01-16 15:10:39 +0800

    Initial commit

---
+++ src/CS_sem2.java
@@ -0,0 +1,54 @@
+public class CS_sem2 {
+
+    public static void main(String[] args) {
+        int myInt = 10;
+        int myInt2 = 4;
+        double myDouble = 2.5;
+        char myChar = 'A';
+        System.out.println(myInt / myDouble);
+        System.out.println(myInt / myInt2);
+        int myInt3 = (int)myDouble;
+        System.out.println(myInt3);
+        System.out.println(12.0 / 0);
+        double myDouble2 = 4.6;
+        double myDouble3 = 4.4;
+        double myDouble4 = 4.5;
+        myChar = 'd';
+        myInt = 66;
+        myDouble2 = myDouble2 + 0.5;
+        int myDouble2int = (int)myDouble2;
+        System.out.println(myDouble2int);
+        myDouble3 = myDouble3 + 0.5;
+        int myDouble3int = (int)myDouble3;
+        System.out.println(myDouble3int);
+        myDouble4 = myDouble4 + 0.5;
+        int myDouble4int = (int)myDouble4;
+        System.out.println(myDouble4int);
+        int myCharint = (int)myChar;
+        System.out.println(myCharint);
+        char myIntchar = (char)myInt;
+        int myInt3 = 7;
+        System.out.println(myIntchar);
+        System.out.println(myInt);
+        System.out.println(myInt++);
+        System.out.println(myInt);
+        System.out.println(++myInt);
+
+
+
+
+
+    }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
